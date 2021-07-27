80accee7ad7de91da807f5e9e29e811e08c31fb1
baraaAlosaily <seelbaraa@gmail.com>
2021-07-27 21:14:24 +0300

    done

---
+++ linter/app/src/main/java/linter/App.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        int lineIncre = 0;
+        Path path = Paths.get("../linter/app/src/main/resources/gates.js");
+        try {
+            List<String> lines = Files.readAllLines(path);
+            for (String line : lines) {
+                lineIncre++;
+                if (!(line.isEmpty())) {
+                    if (line.contains("if") || line.contains("else")||line.contains(";")||line.contains("}")||line.contains("{")) {
+                    } else {
+                        System.out.println("line" + lineIncre + " missing simicolon.");
+                    }
+                }
+            }
+        } catch (IOException exception) {
+            System.out.println("An error occurred while reading from file");
+        }
+
+    }
+}
